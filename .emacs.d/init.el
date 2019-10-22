76096604299ba45e14478ccf804345912f7e3d8b
Mansour Ghaoui <m.ghaoui@interpulse.nl>
2019-10-22 13:32:27 +0200

    helm

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -22,7 +22,10 @@ There are two things you can do about this warning:
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(inhibit-startup-screen t))
+ '(inhibit-startup-screen t)
+ '(package-selected-packages
+   (quote
+    (helm web-mode php-mode flycheck editorconfig better-defaults))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -46,6 +49,10 @@ There are two things you can do about this warning:
 
 ;; flycheck
 (global-flycheck-mode)
+
+;; helm
+(require 'helm)
+(global-set-key (kbd "M-x") 'helm-M-x)
 
 ;; Basic settings
 
