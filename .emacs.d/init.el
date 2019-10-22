4da565ce1c44ce2d63e9a683d2d2cf112a2f3527
Moni Ghaoui <moni@localhost.localdomain>
2019-10-22 20:45:11 +0200

    Font

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -30,12 +30,13 @@ There are two things you can do about this warning:
  '(package-selected-packages
    (quote
     (color-theme-sanityinc-tomorrow solarized-theme company projectile helm web-mode php-mode flycheck editorconfig better-defaults))))
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:family "Source Code Pro" :foundry "ADBO" :slant normal :weight normal :height 113 :width normal)))))
 
 ;; better-defaults
 (require 'better-defaults)
